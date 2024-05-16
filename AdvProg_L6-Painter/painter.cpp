52715dcc3b0fae833d4f0a40f12d994f75f47727
hungdung29 <dung292005@gmail.com>
2024-05-16 23:06:11 +0700

    update painter

--- AdvProg_L6-Painter/painter.cpp
+++ AdvProg_L6-Painter/painter.cpp
@@ -50,7 +50,7 @@ void Painter::jumpBackward(int numPixel)
 void Painter::turnLeft(double degree)
 {
     // TODO: rotate left the painter 
-    angle += degree;  
+    angle -= degree;  
     angle = fmod(angle,360.0);
 }
 
